a444e5c9f00a267fddd575b0d556c398b686c4ec
Zheng Lai <zhenglai3@gmail.com>
2023-01-04 21:36:36 -0500

    Add issues controller

---
+++ ZangDesk.API/Controllers/ValuesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ZangDesk.API.Controllers
+{
+    [ApiController]
+    [Route("api/issues")]
+    public class IssuesController : ControllerBase
+    {
+        [HttpGet]
+        public JsonResult GetIssues()
+        {
+            return new JsonResult(
+                new List<object>
+                {
+                    new { id = 1, Name = "Issue 1" },
+                    new { id = 2, Name = "Issue 2" }
+                });
+        }
+    }
+}
