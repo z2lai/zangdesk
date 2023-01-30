e70b459c0b11f6665460ff4272040212fc68bfae
Zheng Lai <zhenglai3@gmail.com>
2023-01-29 20:42:55 -0500

    add Delete action method

--- ZangDesk.API/Controllers/IssuesController.cs
+++ ZangDesk.API/Controllers/IssuesController.cs
@@ -49,14 +49,28 @@ namespace ZangDesk.API.Controllers
             return CreatedAtAction(nameof(GetById), new { id = issue.Id }, issue); // https://ochzhen.com/blog/created-createdataction-createdatroute-methods-explained-aspnet-core#createdataction-explained
         }
 
-        [HttpPut]
+        [HttpPut("{id}")]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<IActionResult> Update(int id, Issue issue)
         {
             if (id != issue.Id) return BadRequest();
 
-            _context.Entry(issue).State = EntityState.Modified;
+            _context.Entry(issue).State = EntityState.Modified; // Change Tracking in EF Core: https://learn.microsoft.com/en-us/ef/core/change-tracking/
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var issueToDelete = await _context.Issues.FindAsync(id);
+            if (issueToDelete == null) return NotFound();
+
+            _context.Issues.Remove(issueToDelete);
             await _context.SaveChangesAsync();
 
             return NoContent();
